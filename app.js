0ad8f7cce3980ba735f7e75a51c34d2be0af072b
hendrimalo <hendri.malo@yahoo.co.id>
2022-02-03 16:53:13 +0700

    dev to prod

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 require('./config');
 
 // import and connect database mongoose
-mongoose.connect(process.env.MONGO_DB_DEV, {
+mongoose.connect(process.env.MONGO_DB_PROD, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
