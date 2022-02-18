bd3ef1ececc9428187dc7ba7782a4959e6c23e4a
hendrimalo <hendri.malo@yahoo.co.id>
2022-02-18 19:02:18 +0700

    fix date local jakarta and dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,20 +1,25 @@
-const { format } = require('date-fns');
 const Reservation = require('../reservation/model');
 const User = require('../user/model');
 const Transaction = require('../transaction/model');
 const Review = require('../review/model');
+const Member = require('../member/model');
+const Product = require('../service/model');
+const date = require('../../utils/date');
 
 module.exports = {
   index: async (req, res) => {
     try {
+      const member = await Member.find({ status: 'Active' });
+      const product = await Product.find().select('name price');
       const reservation = await Reservation.find({
-        date: format(new Date(), 'yyyy-MM-dd'),
+        date: date.dateJakarta(),
       }).sort({ date: -1, time: -1 });
       const todayReservation = await Reservation.find({
-        date: format(new Date(), 'yyyy-MM-dd'),
+        date: date.dateJakarta(),
       }).count();
       const userTotal = await User.find().count();
       const todayIncome = await Transaction.aggregate([
+        { $match: { date: date.dateJakarta() } },
         { $group: { _id: null, total: { $sum: '$total' } } },
       ]);
       const rating = await Review.aggregate([
@@ -31,6 +36,8 @@ module.exports = {
         username: req.session.user.username,
         role: req.session.user.role,
         reservation,
+        product,
+        member,
         todayReservation,
         todayIncome,
         userTotal,
---
+++ utils/date.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+const momentTZ = require('moment-timezone');
+
+function getDates(startDate, stopDate) {
+  const dateArray = [];
+  let currentDate = moment(startDate);
+  const targetDate = moment(stopDate);
+  while (currentDate <= targetDate) {
+    dateArray.push(moment(currentDate).format('YYYY-MM-DD'));
+    currentDate = moment(currentDate).add(1, 'days');
+  }
+  return dateArray;
+}
+
+function dateJakarta() {
+  return momentTZ(new Date()).tz('Asia/Jakarta').format('YYYY-MM-DD');
+}
+
+module.exports = { getDates, dateJakarta };
