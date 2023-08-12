10adc867eb988bf5141de989c5c08f832b527aa2
hendrimalo <hendri.malo@yahoo.co.id>
2023-08-12 23:36:03 +0700

    menghaps config tidak terpakai

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-  jwtKey: process.env.SECRET,
-  mongo_dev: process.env.MONGO_DB_DEV,
-  mongo_prod: process.env.MONGO_DB_PROD,
-};
